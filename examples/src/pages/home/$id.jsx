ea212d8b898013ce77c9e7c63e059cbc48bd9121
limingyang <865713897@qq.com>
2025-01-07 10:30:29 +0800

    chore(): 更改构建模式

--- examples/src/pages/home/$id.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HomeId() {
-  return (
-    <div>
-      <h1>This is Home Id Page</h1>
-    </div>
-  );
-}
--- examples/src/pages/login/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Login() {
-  return (
-    <div>
-      <h1>This is Login Page</h1>
-    </div>
-  );
-}
